81602b8befa98142f5074bec3d8b94a6fbd4832a
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-06-01 20:08:24 +0600

    search by name

--- components/Countries.js
+++ components/Countries.js
@@ -3,18 +3,29 @@ import React, { useEffect, useState } from "react";
 import Country from "./Country";
 
 export default function Countries() {
-  const [countries, SetCountries] = useState();
+  const [countries, SetCountries] = useState([]);
+  const [searched, setSearched] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => SetCountries(data));
+      .then((data) => {
+        setSearched(data);
+        SetCountries(data);
+      });
   }, []);
+
+  const handleSearch = (text) => {
+    const filter = countries.filter((country) =>
+      country.name.common.includes(text)
+    );
+    setSearched(filter);
+  };
   return (
     <View style={{ marginTop: 20, padding: 20 }}>
       <ScrollView>
-        <Text style={styles.header}>Countries: {countries?.length}</Text>
-        <TextInput style={styles.input}></TextInput>
-        {countries?.map((country, index) => (
+        <Text style={styles.header}>Countries: {searched?.length}</Text>
+        <TextInput onChangeText={handleSearch} style={styles.input}></TextInput>
+        {searched?.map((country, index) => (
           <Country key={index} country={country}></Country>
         ))}
       </ScrollView>
